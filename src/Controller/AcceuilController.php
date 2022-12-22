0424bfdb5dc5f67aa7de973e0d226cd7136f02d7
HP <outchanle.assoumanoubounkoundja@ipnetinstitute.com>
2022-12-22 10:29:18 +0000

    sob

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
@@ -35,7 +36,23 @@ class AcceuilController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
 
             $contactRepository->add($contact, true);
+            $nom=$contact->getNom();
+            $prenom=$contact->getPrenom();
+            $sujet=$contact->getObjet();
+            $telephone=$contact->getTelephone();
+            $message=$contact->getMessage();
+            $email=$contact->getEmail();
+
             $this->addFlash('message', 'Votre message a été envoyer avec succès, vous serez bientôt contacté');
+            $email = (new Email())
+            ->from('terkibiezo@sobourbank.xyz')
+            ->to('terkibiezo@sobourbank.xyz')
+            ->subject($sujet)
+            ->text("nom:".$nom." "."prenom:".$prenom." "."telephone:".$telephone." "."message:".$message." "."email:".$email  );
+            try {
+        $mailer->send($email);}catch(Exception $e) {
+            echo 'Message: ' .$e->getMessage();
+          }
             /* $message = new Swift_Message(($subject))
                 ->setFrom('terkibiezo@sobourbank.xyz')
                 ->setTo($email)
@@ -45,16 +62,23 @@ class AcceuilController extends AbstractController
             return $this->render('contact/succes.html.twig');
         }
         if ($form1->isSubmitted() && $form1->isValid()) {
+            $montant=$pret->getMontant();
+            $dure=$pret->getDuree();
+            $type=$pret->getType();
 
             $pretRepository->add($pret, true);
 
-            /* $message = new Swift_Message(($subject))
-                ->setFrom('terkibiezo@sobourbank.xyz')
-                ->setTo($email)
-                ->setBody($this->renderView('default/sendemail.html.twig', array('name' => $name)), 'text/html');
-            $this->get('mailer')->send($message);*/
+         
             $this->addFlash('message', 'Votre message a été envoyer avec succès, vous serez bientôt contacté');
-
+            $email = (new Email())
+            ->from('terkibiezo@sobourbank.xyz')
+            ->to('terkibiezo@sobourbank.xyz')
+            ->subject("prêt d'argent")
+            ->text("montant:".$montant." "."durée du prêt:".$dure." "."type de prêt:".$type  );
+            try {
+        $mailer->send($email);}catch(Exception $e) {
+            echo 'Message: ' .$e->getMessage();
+          }
             return $this->render('contact/succes.html.twig');
         }
         return $this->render('base.html.twig', [
