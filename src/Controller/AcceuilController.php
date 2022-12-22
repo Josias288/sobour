40bb6ca41dc7d359d01b53d21a8eae4159826f66
Josias288 <outchanle.assoumanoubounkoundja@ipnetinstitute.com>
2022-12-22 23:05:27 +0000

    update message.el

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -36,23 +36,24 @@ class AcceuilController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
 
             $contactRepository->add($contact, true);
-            $nom=$contact->getNom();
-            $prenom=$contact->getPrenom();
-            $sujet=$contact->getObjet();
-            $telephone=$contact->getTelephone();
-            $message=$contact->getMessage();
-            $email=$contact->getEmail();
+            $nom = $contact->getNom();
+            $prenom = $contact->getPrenom();
+            $sujet = $contact->getObjet();
+            $telephone = $contact->getTelephone();
+            $message = $contact->getMessage();
+            $email = $contact->getEmail();
 
             $this->addFlash('message', 'Votre message a été envoyer avec succès, vous serez bientôt contacté');
             $email = (new Email())
-            ->from('terkibiezo@sobourbank.xyz')
-            ->to('terkibiezo@sobourbank.xyz')
-            ->subject($sujet)
-            ->text("nom:".$nom." "."prenom:".$prenom." "."telephone:".$telephone." "."message:".$message." "."email:".$email  );
+                ->from('terkibiezo@sobourbank.xyz')
+                ->to('terkibiezo@sobourbank.xyz')
+                ->subject($sujet)
+                ->text("nom:" . $nom . " " . "prenom:" . $prenom . " " . "telephone:" . $telephone . " " . "message:" . $message . " " . "email:" . $email);
             try {
-        $mailer->send($email);}catch(Exception $e) {
-            echo 'Message: ' .$e->getMessage();
-          }
+                $mailer->send($email);
+            } catch (Exception $e) {
+                echo 'Message: ' . $e->getMessage();
+            }
             /* $message = new Swift_Message(($subject))
                 ->setFrom('terkibiezo@sobourbank.xyz')
                 ->setTo($email)
@@ -62,23 +63,27 @@ class AcceuilController extends AbstractController
             return $this->render('contact/succes.html.twig');
         }
         if ($form1->isSubmitted() && $form1->isValid()) {
-            $montant=$pret->getMontant();
-            $dure=$pret->getDuree();
-            $type=$pret->getType();
+            $montant = $pret->getMontant();
+            $dure = $pret->getDuree();
+            $type = $pret->getType();
+            $nom = $pret->getNom();
+            $telephone = $pret->getTelephone();
+            $email = $pret->setEmail();
 
             $pretRepository->add($pret, true);
 
-         
+
             $this->addFlash('message', 'Votre message a été envoyer avec succès, vous serez bientôt contacté');
             $email = (new Email())
-            ->from('terkibiezo@sobourbank.xyz')
-            ->to('terkibiezo@sobourbank.xyz')
-            ->subject("prêt d'argent")
-            ->text("montant:".$montant." "."durée du prêt:".$dure." "."type de prêt:".$type  );
+                ->from('terkibiezo@sobourbank.xyz')
+                ->to('terkibiezo@sobourbank.xyz')
+                ->subject("prêt d'argent")
+                ->text("montant:" . $montant . " " . "durée du prêt:" . $dure . " " . "type de prêt:" . $type . " " . "nom:" . $nom . " " . "telephone:" . $telephone . " " . "email:" . $email);
             try {
-        $mailer->send($email);}catch(Exception $e) {
-            echo 'Message: ' .$e->getMessage();
-          }
+                $mailer->send($email);
+            } catch (Exception $e) {
+                echo 'Message: ' . $e->getMessage();
+            }
             return $this->render('contact/succes.html.twig');
         }
         return $this->render('base.html.twig', [
